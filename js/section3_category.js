15cfc26d6ed623ee34f9cd1c0faee9832ae56697
LeeKeonJong <93234774+LeeKeonJong@users.noreply.github.com>
2022-01-21 15:48:25 +0900

    Update section3_category.js

--- js/section3_category.js
+++ js/section3_category.js
@@ -2,27 +2,27 @@ $(function(){
 
     $(".ctg1").click(function(){
         $(".section3_menu1").css("display","block");
-        $(".menu2").css("display","none");
-        $(".menu3").css("display","none");
-        $(".menu4").css("display","none");
+        $(".section3_menu2").css("display","none");
+        $(".section3_menu3").css("display","none");
+        $(".section3_menu4").css("display","none");
       });
       $(".ctg2").click(function(){
         $(".section3_menu2").css("display","block");
-        $(".menu1").css("display","none");
-        $(".menu2").css("display","none");
-        $(".menu3").css("display","none");
+        $(".section3_menu1").css("display","none");
+        $(".section3_menu3").css("display","none");
+        $(".section3_menu4").css("display","none");
       }); 
       $(".ctg3").click(function(){
         $(".section3_menu3").css("display","block");
-        $(".menu1").css("display","none");
-        $(".menu2").css("display","none");
-        $(".menu4").css("display","none");
+        $(".section3_menu1").css("display","none");
+        $(".section3_menu2").css("display","none");
+        $(".section3_menu4").css("display","none");
       }); 
       $(".ctg4").click(function(){
         $(".section3_menu4").css("display","block");
-        $(".menu1").css("display","none");
-        $(".menu2").css("display","none");
-        $(".menu3").css("display","none");
+        $(".section3_menu1").css("display","none");
+        $(".section3_menu2").css("display","none");
+        $(".section3_menu3").css("display","none");
       });
 
 });
