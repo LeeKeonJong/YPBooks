b1d6d40d70ef416a0e441af13e1e62b5433cb9e2
LeeKeonJong <93234774+LeeKeonJong@users.noreply.github.com>
2022-01-21 16:31:28 +0900

    업데이트

--- js/section3_category.js
+++ js/section3_category.js
@@ -9,6 +9,12 @@ $(function(){
         $(".ctg2").removeClass("active2");
         $(".ctg3").removeClass("active2");
         $(".ctg4").removeClass("active2");
+        $(".ctg1").css({"min-height":47});
+        $(".ctg1").css("border-bottom","none");
+        $(".ctg2").css({"border-bottom":"1px solid #b1b1b1"});
+        $(".ctg3").css({"border-bottom":"1px solid #b1b1b1"});
+        $(".ctg4").css({"border-bottom":"1px solid #b1b1b1"});
+        
       });
       $(".ctg2").click(function(){
         $(".section3_menu2").css("display","block");
@@ -17,8 +23,15 @@ $(function(){
         $(".section3_menu4").css("display","none");
         $(".ctg2").addClass("active2");
         $(".ctg1").removeClass("active2");
-        $(".ctg2").removeClass("active2");
         $(".ctg3").removeClass("active2");
+        $(".ctg4").removeClass("active2");
+        $(".ctg2").css({"min-height":47});
+        $(".ctg2").css("border-bottom","none");
+        $(".ctg1").css({"border-bottom":"1px solid #b1b1b1"});
+        $(".ctg3").css({"border-bottom":"1px solid #b1b1b1"});
+        $(".ctg4").css({"border-bottom":"1px solid #b1b1b1"});
+        
+        
       }); 
       $(".ctg3").click(function(){
         $(".section3_menu3").css("display","block");
@@ -29,6 +42,12 @@ $(function(){
         $(".ctg1").removeClass("active2");
         $(".ctg2").removeClass("active2");
         $(".ctg4").removeClass("active2");
+        $(".ctg3").css({"min-height":47});
+        $(".ctg3").css("border-bottom","none");
+        $(".ctg1").css({"border-bottom":"1px solid #b1b1b1"});
+        $(".ctg2").css({"border-bottom":"1px solid #b1b1b1"});
+        $(".ctg4").css({"border-bottom":"1px solid #b1b1b1"});
+        
       }); 
       $(".ctg4").click(function(){
         $(".section3_menu4").css("display","block");
@@ -39,6 +58,12 @@ $(function(){
         $(".ctg1").removeClass("active2");
         $(".ctg2").removeClass("active2");
         $(".ctg3").removeClass("active2");
+        $(".ctg4").css({"min-height":47});
+        $(".ctg4").css("border-bottom","none");
+        $(".ctg1").css({"border-bottom":"1px solid #b1b1b1"});
+        $(".ctg2").css({"border-bottom":"1px solid #b1b1b1"});
+        $(".ctg3").css({"border-bottom":"1px solid #b1b1b1"});
+        
       });
 
 });
